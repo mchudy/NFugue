144a23d4e37a4b42305f9fb989e3271a0ffcbe3b
Marcin Chudy <marcinchudy94@gmail.com>
2016-08-09 21:47:21 +0200

    Add projects for Staccato

---
+++ src/Staccato/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staccato
+{
+    public class Class1
+    {
+    }
+}
