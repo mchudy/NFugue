747254208a78fb6c535a3c85d4778a75d0ae4bb9
Marcin Chudy <marcinchudy94@gmail.com>
2016-09-24 15:47:22 +0200

    Add manual tests for rhythms

---
+++ tests/NFugue.ManualTests/Tests/RhythmTests.cs
@@ -0,0 +1,32 @@
+﻿using NFugue.ManualTests.Utils;
+using NFugue.Patterns;
+using NFugue.Playing;
+using NFugue.Rhythms;
+
+namespace NFugue.ManualTests.Tests
+{
+    public class RhythmTests
+    {
+        private readonly Rhythm rhythm = new Rhythm();
+        private readonly Player player = new Player();
+
+        [ManualTest("Rhythm timing", "The beats in this rhythm should not sound choppy or out-of-synch.")]
+        public void RhythmTimingTest()
+        {
+            rhythm.AddLayer("O..oO...O..oOO..");
+            rhythm.AddLayer("..S...S...S...S.");
+            rhythm.AddLayer("````````````````");
+            rhythm.AddLayer("...............+");
+
+            Pattern pattern = rhythm.GetPattern().Repeat(4);
+            player.Play(pattern);
+        }
+
+        [ManualTest("Two tone rhythm", "The sounds in this beat should not sound choppy or out-of-synch.")]
+        public void TwoToneRhythmTest()
+        {
+            rhythm.AddLayer("oxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxoxo");
+            player.Play(rhythm);
+        }
+    }
+}
