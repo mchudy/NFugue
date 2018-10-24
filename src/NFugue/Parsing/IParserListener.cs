3b4cf2aeacd9c0d99bfab12f09c1031083db25a8
Shinoka <xshinoka@gmail.com>
2018-10-24 15:37:05 +0800

    FIX: Finish C# doc comment

--- src/NFugue/Parsing/IParserListener.cs
+++ src/NFugue/Parsing/IParserListener.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NFugue.Theory;
+using System;
 
 namespace NFugue.Parsing
 {
@@ -17,10 +18,10 @@ namespace NFugue.Parsing
         void AfterParsingFinished(object sender, EventArgs e);
 
         /// <summary>
-        /// Called when the parser encounters a new track (also known as a channel; previously in JFugue,
+        /// Called when the parser encounters a new track (also known as a channel; previously in NFugue,
         /// known as a Voice). Tracks correspond to MIDI tracks/channels.
         /// </summary>
-        /// <param name="e">track the new track event that has been parsed</paramref>
+        /// <param name="e">the new track event that has been parsed</param>
         void TrackChanged(object sender, TrackChangedEventArgs e);
 
         /// <summary>
@@ -28,23 +29,21 @@ namespace NFugue.Parsing
         /// A layer deals with polyphony within a track. While any track may have layers, layers are intended
         /// for use with the percussion track, where each layer may represent notes for a specific percussive instrument.
         /// Layers can essentially be thought of as a "track within a track." Each layer maintains its own 
-        /// time progression, so "L1 Eq Eq L2 Cq Gq" would be the same as saying "Eq+Cq Eq+Gq". Layers are a JFugue
+        /// time progression, so "L1 Eq Eq L2 Cq Gq" would be the same as saying "Eq+Cq Eq+Gq". Layers are a NFugue
         /// feature, and are not a part of the MIDI specification.
         /// </summary>
-        /// <param name="?" newTrack the new track event that has been parsed
-        /// @param oldTrack the previous track. The default value for oldTrack is 0.
         void LayerChanged(object sender, LayerChangedEventArgs e);
 
         /// <summary>
         /// Called when the parser encounters a new instrument selection.
-        /// @param instrument the MIDI instrument value that has been parsed
         /// </summary>
+        /// <param name="e">the MIDI instrument value that has been parsed</param>
         void InstrumentParsed(object sender, InstrumentParsedEventArgs e);
 
         /// <summary>
         /// Called when the parser encounters a new tempo selection.
-        /// @param tempoBPM The new tempo value
         /// </summary>
+        /// <param name="e">The new tempo value</param>
         void TempoChanged(object sender, TempoChangedEventArgs e);
 
         void KeySignatureParsed(object sender, KeySignatureParsedEventArgs e);
@@ -62,8 +61,8 @@ namespace NFugue.Parsing
         /// you will want to care about this if you're counting measures, but this should
         /// have no effect on the rendering of a parsed piece of music.
         /// 
-        /// @param id This is the id of the measure, which is an optional numeric value following the bar character. 
         /// </summary>
+        /// <param name="e">This is the id of the measure, which is an optional numeric value following the bar character.</param>
         void BarLineParsed(object sender, BarLineParsedEventArgs e);
 
         void TrackBeatTimeBookmarked(object sender, TrackBeatTimeBookmarkEventArgs e);
@@ -109,14 +108,14 @@ namespace NFugue.Parsing
 
         /// <summary>
         /// We may have actually parsed a musical note!
-        /// In previous versions of JFugue, ParserListener had separate listeners for parallel notes and sequential notes
+        /// In previous versions of NFugue, ParserListener had separate listeners for parallel notes and sequential notes
         /// (now termed harmonic and melodic notes, respectively)
-        /// In this version of JFugue, whether a note is the first note, a harmonic note, or a melodic note is kept
+        /// In this version of NFugue, whether a note is the first note, a harmonic note, or a melodic note is kept
         /// as a property on the Note object itself.
         /// 
-        /// @param note The note that was parsed. Please see the Note class for more details about notes!
-        /// @see Note
         /// </summary>
+        /// <param name="e">note The note that was parsed. Please see the Note class for more details about notes!</param>
+        /// <seealso cref="Note"/>
         void NoteParsed(object sender, NoteEventArgs e);
 
         void ChordParsed(object sender, ChordParsedEventArgs e);
